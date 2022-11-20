eedc95ee4a67c656a8ac61f2318534b5d762792a
infinixs5lite <kelvinchinedu97@gmail.com>
2022-11-20 22:37:53 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 30
 
@@ -37,6 +40,10 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock
 
 # Health Hal
 PRODUCT_PACKAGES += \
