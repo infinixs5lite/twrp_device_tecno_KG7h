2e5972febd7003852046b53f1ce93d9ed46521c6
infinixs5lite <kelvinchinedu97@gmail.com>
2022-12-30 06:38:57 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,13 +7,6 @@
 
 LOCAL_PATH := device/tecno/KG7h
 
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
@@ -46,23 +39,5 @@ PRODUCT_PACKAGES += \
     mtk_decrypt \
     mtk_decrypt_fbe
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
-# Health Hal
-PRODUCT_PACKAGES += \
-     android.hardware.health@2.1-impl \
-     android.hardware.health@2.1-service
-
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-        ro.crypto.allow_encrypt_override=true \
-	ro.crypto.dm_default_key.options_format.version=2 \
-	ro.crypto.volume.filenames_mode=aes-256-cts \
-	ro.crypto.volume.metadata.method=dm-default-key \
-	ro.crypto.volume.options=::v2
